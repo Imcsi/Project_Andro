8052b7b43361450daefea58ea1292426b916fb40
Imola <imola.demeter@yahoo.com>
2018-11-17 09:22:08 +0200

    Fragmentek létrhozása

--- app/src/main/java/com/example/imola/project/MainActivity.java
+++ app/src/main/java/com/example/imola/project/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.imola.project;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.graphics.drawable.Drawable;
+import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.SpinnerAdapter;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +18,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void ChangeFragment(View view)
+    {
+        Fragment fragment;
+
+        if (view == findViewById(R.id.button1))
+        {
+            fragment = new FragmentOne();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft= fm.beginTransaction();
+            ft.replace(R.id.fragment_place, fragment);
+            ft.commit();
+        }
+        if (view == findViewById(R.id.button2))
+        {
+            fragment = new FragmentTwo();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft= fm.beginTransaction();
+            ft.replace(R.id.fragment_place, fragment);
+            ft.commit();
+        }
+    }
+
+
 }
