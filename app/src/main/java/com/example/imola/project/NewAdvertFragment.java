e6d4856ffc3479e22aa755f0a3bcef792e261db9
Imola <imola.demeter@yahoo.com>
2018-12-20 15:51:32 +0200

    Hirdetes feltoltese keppel

--- app/src/main/java/com/example/imola/project/NewAdvertFragment.java
+++ app/src/main/java/com/example/imola/project/NewAdvertFragment.java
@@ -8,11 +8,13 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.MimeTypeMap;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
@@ -30,44 +32,77 @@ import com.squareup.picasso.Picasso;
 
 public class NewAdvertFragment extends Fragment {
 
-    private static final int PICK_IMAGE_REQUEST= 1;
-    private static final int RESULT_OK= -1;
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private static final int RESULT_OK = -1;
+
+    private EditText mTitle;
+    private EditText mShort_Description;
+    private EditText mLong_Description;
+    private EditText mPhone_Number;
+    private EditText mLocation_Text;
+
     private Button mAddPhoto;
     private Button mbuttonSaveAdvert;
     private ImageView mImageView;
+    private Upload upload;
+
+    private ProgressBar mProgressBar;  //??kell?
+
     private Uri mImageUri;
-    private Uri uri;
-    private ProgressBar mProgressBar;
+
+    private Integer advertNumber = 0;
+
 
     private StorageReference mStorageRef;
     private DatabaseReference mDatabaseRef;
 
+
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
+
+
+
+
         // Inflate the layout for this fragment
-        View view= inflater.inflate(R.layout.fragment_new_advert, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_new_advert, container, false);
 
         mAddPhoto = view.findViewById(R.id.add_photo);
-        mbuttonSaveAdvert=view.findViewById(R.id.buttonSaveAdvert);
-        mImageView= view.findViewById(R.id.imageAdvert);
-        mProgressBar=view.findViewById(R.id.progressBar);
+        mbuttonSaveAdvert = view.findViewById(R.id.buttonSaveAdvert);
+        mImageView = view.findViewById(R.id.imageAdvert);
+        mProgressBar = view.findViewById(R.id.progressBar);  //??
+
+
+        mTitle = view.findViewById(R.id.title);
+        mShort_Description = view.findViewById(R.id.short_description);
+        mLong_Description = view.findViewById(R.id.long_description);
+        mPhone_Number = view.findViewById(R.id.phone_number);
+        mLocation_Text = view.findViewById(R.id.location_text);
 
         mStorageRef = FirebaseStorage.getInstance().getReference("uploads");
-        mDatabaseRef = FirebaseDatabase.getInstance().getReference("uploads");
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("adverts");
+
 
         mAddPhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openFileChooser();
+                //imageUpload();
             }
         });
+
 
 
         mbuttonSaveAdvert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                uploadFile();
+                uploadAdvert();
             }
         });
 
@@ -77,7 +112,11 @@ public class NewAdvertFragment extends Fragment {
 
     }
 
-    private void  openFileChooser(){
+
+
+
+
+    private void openFileChooser() {
         Intent intent = new Intent();
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
@@ -85,13 +124,12 @@ public class NewAdvertFragment extends Fragment {
     }
 
 
-
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null &&
-            data.getData() != null){
+                data.getData() != null) {
 
 
             mImageUri = data.getData();
@@ -102,35 +140,47 @@ public class NewAdvertFragment extends Fragment {
     }
 
 
-    private String getFileExtension(Uri uri){
-            ContentResolver cR = getActivity().getContentResolver();
-            MimeTypeMap mime = MimeTypeMap.getSingleton();
-            return mime.getExtensionFromMimeType(cR.getType(uri));
+    private String getFileExtension(Uri uri) {
+        ContentResolver cR = getActivity().getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cR.getType(uri));
     }
 
-    private void uploadFile(){
-        if (mImageUri != null)
-        {
-            StorageReference fileReference = mStorageRef.child(System.currentTimeMillis()+"."+ getFileExtension(mImageUri));
+
+    private void uploadAdvert() {
+
+
+
+        String advert_Title = mTitle.getText().toString();
+        String advert_Short_Description = mShort_Description.getText().toString();
+        String advert_Long_Description = mLong_Description.getText().toString();
+        String advert_Phone_Number = mPhone_Number.getText().toString();
+        String advert_Location_Text = mLocation_Text.getText().toString();
+
+        if (!TextUtils.isEmpty(advert_Title) && !TextUtils.isEmpty(advert_Short_Description) && !TextUtils.isEmpty(advert_Phone_Number) &&
+                !TextUtils.isEmpty(advert_Long_Description) && !TextUtils.isEmpty(advert_Location_Text) && mImageUri != null) {
+
+            final StorageReference fileReference = mStorageRef.child(System.currentTimeMillis() + "." + getFileExtension(mImageUri));
             fileReference.putFile(mImageUri)
                     .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                    Handler handler = new Handler();
-                    handler.postDelayed(new Runnable() {
+
                         @Override
-                        public void run() {
-                            mProgressBar.setProgress(0);
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Handler handler = new Handler();
+                            handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    mProgressBar.setProgress(0);
+                                }
+                            }, 500);
+
+                            Toast.makeText(getActivity(), "Upload Successful Image", Toast.LENGTH_LONG).show();
+                            //Upload upload2 = new Upload();
+                            // String uploadId = mDatabaseRef.push().getKey();
+                            //mDatabaseRef.child().setValue(upload2);
+
                         }
-                    }, 500);
-
-                    Toast.makeText(getActivity(), "Upload Successful", Toast.LENGTH_LONG).show();
-                    Upload upload=new Upload();
-                    String uploadId =  mDatabaseRef.push().getKey();
-                    mDatabaseRef.child(uploadId).setValue(upload);
-
-                }
-            })
+                    })
                     .addOnFailureListener(new OnFailureListener() {
                         @Override
                         public void onFailure(@NonNull Exception e) {
@@ -141,15 +191,25 @@ public class NewAdvertFragment extends Fragment {
                         @Override
                         public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
 
-                            double progress = (100.0 * taskSnapshot.getBytesTransferred()/ taskSnapshot.getTotalByteCount());
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
                             mProgressBar.setProgress((int) progress);
                         }
                     });
-        }
 
-        else {
-            Toast.makeText(getActivity(),"No file selected", Toast.LENGTH_SHORT).show();
+
+
+            String mPhone_Number = mDatabaseRef.push().getKey();
+
+            upload = new Upload(fileReference.toString(), advert_Title, advert_Short_Description, advert_Long_Description, advert_Phone_Number, advert_Location_Text);
+            mDatabaseRef.child("adverts" + ++advertNumber).setValue(upload);
+
+
+
+            Toast.makeText(getActivity(), "Upload Successful", Toast.LENGTH_LONG).show();
+
+
         }
     }
-
 }
+
+
